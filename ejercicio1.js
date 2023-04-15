6a0d5ed2d7bac107a1d7cbc928f8f3ea65913c0a
JoseM <heathens92@protonmail.ch>
2023-04-15 11:07:20 -0400

    ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+const misDatos = [
+    nombre=  "José",
+    edad = 30,
+    dev = true,
+    fechaDeNacimiento = new Date (2022, 10, 15),
+    libroFavorito = {
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Marquez",
+        fecha: new Date (1985, 3, 25),
+        url: "http://amazon.com/cien_años_de_soledad"
+    }
+]
+console.log(misDatos)
+console.log(misDatos.dev)
